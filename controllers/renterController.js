64543d851e9102562a488b08b134222b9cacc391
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-11-17 22:41:54 +0700

    fix

--- controllers/renterController.js
+++ controllers/renterController.js
@@ -2,14 +2,13 @@ const Renter = require('../models/renterModel');
 
 module.exports.renterSignup = async (req, res) => {
     const renter = new Renter(req.body);
-    // try {
-    //     await renter.save()
-    //     const token = await renter.generateAuthToken()
-    //     res.status(201).send({ renter, token })
-    // } catch (e) {
-    //     res.status(400).send(e)
-    // }
-    res.send(renter)
+    try {
+        // await renter.save()
+        const token = await renter.generateAuthToken()
+        res.status(201).send({ renter, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 };
 
 module.exports.renterLogin = async (req, res) => {
