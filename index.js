134f1a979cba51dfc3e94f56aa5935d57f21335a
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-11-20 21:29:14 +0700

    update owner login

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(renterRouter)
+app.use(ownerRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
