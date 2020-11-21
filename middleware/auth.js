4cf011835de4a60dff805417d98ff802ae155c2f
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-11-21 10:55:05 +0700

    update owner login

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const Renter = require('../models/renterModel');
+const Owner = require('../models/ownerModel');
 
 const auth = async (req, res, next) => {
     try {
@@ -7,13 +8,14 @@ const auth = async (req, res, next) => {
         console.log(token)
         const decoded = jwt.verify(token, 'kenji')
         const renter = await Renter.findOne({ _id: decoded._id, 'tokens.token': token })
-
-        if (!renter) {
+        const owner = await Owner.findOne({ _id: decoded._id, 'tokens.token': token })
+        if (!renter && !owner) {
             throw new Error()
         }
 
         req.token = token
-        req.renter = renter
+        if(renter && !owner) req.renter = renter
+        else if(!renter && owner) req.owner = owner
         next()
     } catch (e) {
         res.status(401).send({ error: 'Please authenticate.' })
