93a2b3d9f298a4d00082317f04b58cc325f82cff
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-11-27 23:50:08 +0700

    update avatar

--- routers/renter.js
+++ routers/renter.js
@@ -25,8 +25,8 @@ router.delete("/renter/profile", auth, renterController.renterDeleteProfile);
 
 router.post("/renter/profile/avatar", auth, async (req, res) => {
   // console.log('new Avatar: ',req.body.avatar)
-  const abc = req
-  res.send(abc)
+  // console.log(req)
+  res.send(req.body)
 });
 
 router.delete("/renter/profile/avatar", auth, async (req, res) => {
