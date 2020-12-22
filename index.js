4da8ef5ba9da934d5f2e2699e8d214b1157eb6d7
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-12-22 10:24:46 +0700

    add public location

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const renterRouter = require('./routers/renter')
 const ownerRouter = require('./routers/owner')
 const adminRouter = require('./routers/admin')
 const accomodRouter = require('./routers/accommodation')
+const locationRouter = require('./routers/location')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -15,6 +16,7 @@ app.use(renterRouter)
 app.use(ownerRouter)
 app.use(adminRouter)
 app.use(accomodRouter)
+app.use(locationRouter)
 
 
 app.listen(port, () => {
