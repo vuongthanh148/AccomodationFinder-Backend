b6914282fed82a0ca16b1e8016ec902e6d3d913b
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-12-27 10:42:47 +0700

    adding follow feature

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ const port = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(cors())
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Methods", "POST");
-    next();
-  });
-  
 app.use(renterRouter)
 app.use(ownerRouter)
 app.use(adminRouter)
