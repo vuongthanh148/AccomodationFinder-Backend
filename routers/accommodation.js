4d1349e7484615571abb191e18f25282a5c3a014
hoangnx30 <xuanhoang30071999@gmail.com>
2020-12-28 14:53:23 +0700

    api approve, delete accommodation

--- routers/accommodation.js
+++ routers/accommodation.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const auth = require('../middleware/auth')
 const accomodController = require('../controllers/accommodationController')
+const Accommodation = require('../models/accommodationModel')
+const Comment = require('../models/commentModel')
 const router = express.Router()
 
 router.post('/accommodation/newAccomod', auth, accomodController.newAccomod)
@@ -8,5 +10,46 @@ router.post('/accommodation/newAccomod', auth, accomodController.newAccomod)
 router.post('/accommodation', accomodController.viewAccomod)
 
 router.get('/accommodation/:id', accomodController.viewAccomodById)
+
+router.put('/accommodation/approve', async (req, res) => {
+  const { accommodationId } = req.body
+  try {
+    const accommodation = await Accommodation.findById(accommodationId)
+    accommodation.pending = false
+    await accommodation.save()
+    res.status(200).json({
+      success: true,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    })
+  }
+})
+
+router.delete('/accommodation', async (req, res) => {
+  const { accommodationId } = req.body
+  try {
+    const accommodation = await Accommodation.findById(accommodationId)
+    const listComment = await Comment.find({ accommodationId })
+    if (listComment.length !== 0) {
+      listComment.forEach(async (comment) => {
+        await comment.remove()
+      })
+    }
+
+    await accommodation.remove()
+    res.status(200).json({
+      success: true,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      success: false,
+    })
+  }
+})
 
 module.exports = router
