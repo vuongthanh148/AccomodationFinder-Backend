f01fa015259f778504d02272e0a6f7e92c280a0c
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-12-29 23:27:46 +0700

    fix

--- routers/follow.js
+++ routers/follow.js
@@ -3,18 +3,18 @@ const router = new express.Router()
 const Follow = require('../models/followModel')
 const auth = require('../middleware/auth')
 
-//Get All Location
-// router.post('/followList', auth, async (req, res) => {
-//   console.log(req.body)
-//   const _id = req.body._id
-//   try {
-//     const list = await Follow.find({ userId: _id })
-//     if (list) res.send(list)
-//     else res.send([])
-//   } catch (e) {
-//     console.log(e)
-//   }
-// })
+
+router.post('/followList', auth, async (req, res) => {
+  console.log(req.body)
+  const _id = req.body._id
+  try {
+    const list = await Follow.find({ userId: _id })
+    if (list) res.send(list)
+    else res.send([])
+  } catch (e) {
+    console.log(e)
+  }
+})
 
 // router.post('/followChange', auth, async (req, res) => {
 //   console.log(req.body)
