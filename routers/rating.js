2fa7bc1ebdec6605f0104fb4b03419b8af117cd7
Kenji <48047759+vuongthanh148@users.noreply.github.com>
2020-12-30 06:20:11 +0700

    fix

--- routers/rating.js
+++ routers/rating.js
@@ -7,10 +7,14 @@ const auth = require('../middleware/auth')
 router.post('/rating', auth, async (req, res) => {
   console.log(req.body)
   const tempRating = await Rating.findOne({accommodationId: req.body.accommodationId})
-  tempRating.rate = {
-    userId: req.body.userId,
-    stars: req.body.ratedStar
+  if(tempRating.rate.find(x => x.userId === req.body.userId) != undefined){
+      tempRating.rate[tempRating.rate.find(x => x.userId === req.body.userId)].stars = req.body.ratedStar
   }
+  else tempRating.rate.push({
+      userId: req.body.userId,
+      stars: req.body.ratedStar,
+      ratedDate: Date.now()
+  })
   try {
     await tempRating.save();
     res.status(200).send('Đánh giá thành công')
